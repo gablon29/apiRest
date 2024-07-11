d9429c10ccc46e520a4aad8032259a5b43193221
gabri <gabriellondero40@gmail.com>
2024-07-11 06:50:25 -0300

    incorporando los test unitarios

--- routes/GET/rqGet.js
+++ routes/GET/rqGet.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 
-
 const routerGet = Router();
 
 // funcion comodin para testear cualquier logica
-routerGet.get("/user:id", (req, res) => {
+routerGet.get("/user", (req, res) => {
   const { id } = req.params;
-  console.log(id)
+  console.log(id);
   res.send("hola mundo");
 });
 
+routerGet.route("/allUsers").get((req, res) => {
+  res.send([]);
+});
 
 export default routerGet;

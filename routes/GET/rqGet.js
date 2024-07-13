4cdadd6e74fb1d0786d16c16a0ae0d0a431c31bf
gablon29 <gabriellondero40@gmail.com>
2024-07-13 12:22:40 -0300

    comenzando con los test DDT

--- routes/GET/rqGet.js
+++ routes/GET/rqGet.js
@@ -5,7 +5,6 @@ const routerGet = Router();
 // funcion comodin para testear cualquier logica
 routerGet.get("/user", (req, res) => {
   const { id } = req.params;
-  console.log(id);
   res.send("hola mundo");
 });
 
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,19 +1,21 @@
 import app from "../app.js";
 import request from "supertest";
 
-describe("GET/user", () => {
-  test("should response with a 200 status code", async () => {
-    const response = await request(app).get("/user").send();
-    expect(response.statusCode).toBe(200);
-  });
+describe("Routes", () => {
+  describe("Get request", () => {
+    test("should response with a 200 status code", async () => {
+      const response = await request(app).get("/user").send();
+      expect(response.statusCode).toBe(200);
+    });
 
-  test("should response with a 404 status code", async () => {
-    const response = await request(app).get("/users:id").send();
-    expect(response.statusCode).toBe(404);
-  });
+    test("should response with a 404 status code", async () => {
+      const response = await request(app).get("/users:id").send();
+      expect(response.statusCode).toBe(404);
+    });
 
-  test("Deberia mostrarme un arreglo", async () => {
-    const response = await request(app).get("/allUsers");
-    expect(response.body).toBeInstanceOf(Array);
+    test("Deberia mostrarme un arreglo", async () => {
+      const response = await request(app).get("/allUsers");
+      expect(response.body).toBeInstanceOf(Array);
+    });
   });
 });

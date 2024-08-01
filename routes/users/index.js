ed30e901792055836bacdaf5db619d145c66e18c
gablon29 <gabriellondero40@gmail.com>
2024-07-31 21:23:21 -0300

    agregando funcionalidad de get user

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUser } from "../../controllers/user/createUser.js";
 import { validateUser } from "../../handlers/userHandlers.js";
+import { getUserByID } from "../../controllers/user/getUserByID.js";
 const router = Router();
 
-router.route("/users").post(validateUser, createUser);
+router.route("/users").post(validateUser, createUser).get(getUserByID);

11191bc99ff3e7f3de7a1396ddf1dbc04ef77676
gablon29 <gabriellondero40@gmail.com>
2024-08-29 21:58:58 -0300

    agregando funcionalidad para cargar el pdf

--- demo.js
+++ demo.js
@@ -1,3 +1,20 @@
-import moment from "moment";
+import cie10Data from "./cie10.json" assert { type: "json" };
 
-const a = moment().format("YYYY-MM-DD HH:mm:ss");
+function groupByCodePrefix(data) {
+  const groupedCodes = {};
+
+  data.forEach((item) => {
+    const prefix = item.code.split(".")[0];
+
+    if (!groupedCodes[prefix]) {
+      groupedCodes[prefix] = [];
+    }
+
+    groupedCodes[prefix].push(item); // Guardamos el objeto completo (código y descripción)
+  });
+
+  return Object.values(groupedCodes);
+}
+
+const groupedCie10Data = groupByCodePrefix(cie10Data);
+console.log(groupedCie10Data);
